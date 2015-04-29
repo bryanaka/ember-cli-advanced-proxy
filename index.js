226d4f38b688afb7bcf0bf8d6602df917ed39587
Bryan M. Robles <bryanaka0@gmail.com>
2015-04-29 15:38:07 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 /* jshint node: true */
 'use strict';
+var AdvancedProxyAddon = require('./lib/ember-cli-advanced-proxy');
 
 module.exports = {
-  name: 'ember-cli-advanced-proxy'
+  name: 'ember-cli-advanced-proxy',
+
+  serverMiddleware: function(config) {
+    var app = config.app;
+    var options = config.options;
+    var proxyAddon = new AdvancedProxyAddon(config);
+    app.stack.forEach(function(middleware) {
+      console.log('\n\n\n');
+      console.log(middleware);
+      console.log('\n\n\n');
+    });
+    // proxyAddon.serverMiddleware({
+    //   app: app,
+    //   options: options
+    // });
+  }
 };
