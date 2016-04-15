594e13cf2de386d8ea65dac88f643241f7a28363
Bryan Robles <bryanaka0@gmail.com>
2016-04-15 15:41:25 -0700

    update for ember 2.4 (#1)
    * transition to Ember 2.4 compatibility and bump to 0.0.7
    
    * improve readme
    
    * 0.0.6
    
    * 0.0.7

--- README.md
+++ README.md
@@ -1,25 +1,16 @@
-# Ember-cli-advanced-proxy
+Ember-cli-advanced-proxy
+================================================================================
 
-This README outlines the details of collaborating on this Ember addon.
+## Disclaimer
 
-## Installation
+Please don't use this addon. It contains a nasty hack that works for us, because
+we can't use Ember CLI's HTTP Mocks API until some changes to Ember CLI
+around how it loads configuration.
 
-* `git clone` this repository
-* `npm install`
-* `bower install`
+We have a somewhat unique situation in how our build system is used, and chances
+are that you can probably just use Ember CLI's HTTP Mocks. Please look at them
+first before you consider using this.
 
-## Running
-
-* `ember server`
-* Visit your app at http://localhost:4200.
-
-## Running Tests
-
-* `ember test`
-* `ember test --server`
-
-## Building
-
-* `ember build`
+[Ember CLI HTTP Mocks](http://ember-cli.com/user-guide/#mocks-and-fixtures)
 
 For more information on using ember-cli, visit [http://www.ember-cli.com/](http://www.ember-cli.com/).
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 /* jshint node: true */
 'use strict';
 var AdvancedProxyAddon = require('./lib/ember-cli-advanced-proxy');
+/*
+* This number represents the position that the proxy middleware holds in
+* Ember CLI's normal express middleware stack.
+*/
+var MIDDLEWARE_TO_REMOVE = 12;
 
 module.exports = {
   name: 'ember-cli-advanced-proxy',
@@ -9,13 +14,13 @@ module.exports = {
     var app = config.app;
     var options = config.options;
     var proxyAddon = new AdvancedProxyAddon(config.project);
-    // TODO: THIS IS A HUGE HACK.
+    // HACK: THIS IS A HUGE HACK.
     // We shouldn't be removing middleware from the stack
     // and it we can match on anonymous functions, so no guarentees
     var middlewareStack = (app._router.stack || [])
-    var middlewareToRemove = 11;
-    if(middlewareStack.length > middlewareToRemove){
-      middlewareStack.splice(middlewareToRemove, 1);
+
+    if(middlewareStack.length > MIDDLEWARE_TO_REMOVE){
+      middlewareStack.splice(MIDDLEWARE_TO_REMOVE, 1);
       app._router.stack = middlewareStack;
     }
     proxyAddon.serverMiddleware({
